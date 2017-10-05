da2f6f35c488fb3cc9ee9c96568aee11b1392d9f
Tim Ninan <tim.ninan@gmail.com>
2017-10-05 18:54:22 -0500

    implemented functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,49 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var newInstance = {};
+  newInstance.storage = {};
+  newInstance.queueSize = 0;
+
+  extend(newInstance, queueMethods);
+  return newInstance;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() { 
+  	return this.queueSize; 
+  },
+  enqueue: function(value) { 
+  	this.storage[this.queueSize] = value; 
+  	this.queueSize++; 
+  },
+  dequeue: function() { 
+  	if (!this.queueSize) {
+  	 return undefined;
+  	}
+  	this.queueSize--;
+  	var dequeued = this.storage[0];
+  	for(var i = 0; i < this.queueSize; i++) {
+  	  this.storage[i] = this.storage[i+1];
+  	}
+  	if (!this.queueSize) {
+  	  this.storage[0] = undefined;
+  	}
+  	return dequeued; 
+  }
+
+};
+
+var extend = function(obj1,obj2) {
+    for (var key in obj2) {
+  	  obj1[key] = obj2[key];
+    }
+  
+};
+ 
+  
+  
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.stackSize = 0;
+  someInstance.storage = {};
+  extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+	size: function() {
+		return this.stackSize;
+	}, 
+	push: function(value) {
+		this.storage[this.stackSize] = value;
+		this.stackSize++;
+	}, 
+	pop: function() {
+		if (this.stackSize > 0) {
+			this.stackSize--;
+			var popped = this.storage[this.stackSize];
+		  delete this.storage[this.stackSize];
+		  return popped;
+		} 
+		return undefined;
+	}
+};
+
+var extend = function(obj1, obj2) {
+	for (var key in obj2) {
+		obj1[key] = obj2[key];
+	}
+}
 
 
