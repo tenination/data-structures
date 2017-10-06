7ffc42c04a4f382bc034b1e447014efe6de00c9f
Tim Ninan <tim.ninan@gmail.com>
2017-10-06 02:21:01 -0500

    implemented linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,46 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = Node();
+  list.tail = Node();
 
   list.addToTail = function(value) {
+    
+   var newNode = Node();
+   newNode.value = value;
+
+
+   if (list.head.value === undefined) {
+     list.head = newNode;
+   }
+
+   var previousNode = list.tail;
+   previousNode.next = newNode;
+   list.tail = newNode;
+    
   };
 
   list.removeHead = function() {
+    var previousHead = list.head;
+    list.head = list.head.next; 
+    return previousHead.value;
   };
 
   list.contains = function(target) {
+    var found = false;
+    var currentNode = list.head;
+    while(currentNode.next !== null && !found) {
+
+      if (currentNode && currentNode.value === target) {
+        found = true;
+      } else {
+        currentNode = currentNode.next;
+      }
+
+    }
+    if (list.tail.value === target) {
+      found = true;
+    }
+    return found;
   };
 
   return list;
